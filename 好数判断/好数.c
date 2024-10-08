9c1b0f6362ded7be67d25eb2eb19e3b895fb4e5f
黄冉阳 <huangranyang@outlook.com>
2024-10-08 08:19:41 +0000

    第一次上传
    Signed-off-by: 黄冉阳 <huangranyang@outlook.com>

---
+++ 好数判断/好数.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int N;
+  scanf("%d",&N);
+	int sum = 0;
+	for (int p = 1; p <= N; p++)
+	{
+		int i = p, num = 1, s = 0,good=1;
+		if (i == 0)
+		{
+			sum++;
+		}
+		else
+		{
+			while (i != 0)
+			{
+				s = i % 10; //取个位 
+				if (s % 2 == 1 && num % 2 == 1)
+				{
+				}
+				else
+				{
+					if (s % 2 == 0 && num % 2 == 0)
+					{
+					}
+					else if (p != 1)
+					{
+						good = 0;
+					}
+				}
+
+
+				i /= 10;
+				num++;
+			}
+			if (good == 1)
+			{
+				sum++;
+			}
+			else
+			{
+			}
+		}
+	}
+	printf("%d", sum);
+  return 0;
+}
